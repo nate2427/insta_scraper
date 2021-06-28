46e513dfdef6c05888724dd4d6405e0cfbbf7928
Street Coder Nate <natebaker2427@gmail.com>
2021-06-28 10:46:00 -0500

    Connected the frontend button click to the backend

--- frontend/src/components/SearchContainer/SearchContainer.jsx
+++ frontend/src/components/SearchContainer/SearchContainer.jsx
@@ -1,13 +1,30 @@
 import React from "react";
+import { useState } from "react";
 import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
 import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
-import Icon from "@material-ui/core/Icon";
 import styles from "./styles";
+import Axios from "axios";
 
 const SearchContainer = () => {
   const classes = styles();
+  const [username, setUsername] = useState(null);
+
+  const handleOnChange = (event) => {
+    const user = event.target.value;
+    setUsername(user);
+  };
+
+  const handleButtonClick = async () => {
+    //   return if blank username
+    if (username === null) return;
+    console.log(username);
+    const data = await Axios.post("/api/get-user-data", {
+      username,
+    });
+    console.log(data);
+  };
 
   return (
     <Grid
@@ -31,7 +48,11 @@ const SearchContainer = () => {
         </Typography>
       </Grid>
       <Grid justify="center" className={classes.inputContainer} container item>
-        <TextField id="standard-basic" label="Username" />
+        <TextField
+          onChange={(event) => handleOnChange(event)}
+          id="standard-basic"
+          label="Username"
+        />
       </Grid>
       <Grid
         md={6}
@@ -47,6 +68,7 @@ const SearchContainer = () => {
           variant="contained"
           color="primary"
           fullWidth
+          onClick={handleButtonClick}
         >
           Send
         </Button>
