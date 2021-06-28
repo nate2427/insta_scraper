7d967ede22e278c419ea0f7f010415ceb2c9bf0f
Street Coder Nate <natebaker2427@gmail.com>
2021-06-28 10:08:50 -0500

    Got da styling down for da search container

--- frontend/src/components/SearchContainer/SearchContainer.jsx
+++ frontend/src/components/SearchContainer/SearchContainer.jsx
@@ -4,22 +4,50 @@ import Typography from "@material-ui/core/Typography";
 import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
 import Icon from "@material-ui/core/Icon";
+import styles from "./styles";
 
 const SearchContainer = () => {
+  const classes = styles();
+
   return (
-    <Grid item container md={10} sm={11} xs={12} className="search-container">
-      <Grid item>
+    <Grid
+      item
+      container
+      md={10}
+      sm={11}
+      xs={12}
+      className="search-container"
+      direction="column"
+      justify="center"
+      alignItems="center"
+    >
+      <Grid justify="center" className={classes.titleContainer} container item>
         <Typography variant="h1">Insta Scraper</Typography>
       </Grid>
-      <Grid item>
-        <Typography variant="body1">Insta Scraper</Typography>
+      <Grid justify="center" className={classes.descContainer} container item>
+        <Typography variant="body1">
+          This will allow us to find a user on insta without even logging into
+          an account.
+        </Typography>
       </Grid>
-      <Grid item>
+      <Grid justify="center" className={classes.inputContainer} container item>
         <TextField id="standard-basic" label="Username" />
       </Grid>
-      <Grid item>
+      <Grid
+        md={6}
+        alignItems="center"
+        justify="center"
+        className={classes.buttonContainer}
+        container
+        item
+      >
         {/* This Button uses a Font Icon, see the installation instructions in the Icon component docs. */}
-        <Button variant="contained" color="primary" endIcon={<Icon>send</Icon>}>
+        <Button
+          className={classes.sendButton}
+          variant="contained"
+          color="primary"
+          fullWidth
+        >
           Send
         </Button>
       </Grid>
---
+++ frontend/src/components/SearchContainer/styles.jsx
@@ -0,0 +1,18 @@
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles(() => ({
+  titleContainer: {
+    padding: "1rem 0",
+  },
+  descContainer: {
+    padding: "1rem 0",
+  },
+  inputContainer: {
+    paddingTop: "5rem",
+  },
+  buttonContainer: {
+    padding: "5rem",
+  },
+}));
+
+export default useStyles;
