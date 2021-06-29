b4e7ee27884147c7327bd2f9626efd9c74317f7a
Street Coder Nate <natebaker2427@gmail.com>
2021-06-29 02:22:56 -0500

    removed the database startup code

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const { connectToDB } = require("./config/db_connection");
 const routes = require("./routes/landing");
 const api_routes = require("./routes/apis/landing");
 const bodyParser = require("body-parser");
@@ -18,17 +17,10 @@ app.use(api_routes);
 app.use(express.static(path.join(__dirname, "frontend/build")));
 
 // connect to the mongo client and after connecting, print message and start the webserver
-connectToDB((err) => {
-  !err
-    ? console.log(
-        "\n\nApp finsihed connecting to the db @ http://localhost:4444"
-      )
-    : process.exit(1);
 
-  // start the webserver
-  app.listen(PORT, () => {
-    console.log(
-      `\nServer started and listening on port ${PORT} --> http://localhost:${PORT}\n`
-    );
-  });
+// start the webserver
+app.listen(PORT, () => {
+  console.log(
+    `\nServer started and listening on port ${PORT} --> http://localhost:${PORT}\n`
+  );
 });
